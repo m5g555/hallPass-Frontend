c261d29f19588fb98547542b1f9fcc480a71f515
msg555 <mitchelg555@gmail.com>
2022-10-14 22:12:05 -0400

    w

--- src/app/new-pass/new-pass.component.ts
+++ src/app/new-pass/new-pass.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// This will be a component that consists of a form for creating a new pass
+// It will be accessed by clicking on the "Create New Pass" button on the home page
+// Will contain fields for student and recieving teacher name
+// Maybe add an optional reason field?
+
 @Component({
   selector: 'app-new-pass',
   templateUrl: './new-pass.component.html',
